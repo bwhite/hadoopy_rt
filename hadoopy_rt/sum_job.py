acddfb83716dd8ec8b52a39922285af5f41a0112
Brandyn A. White <bwhite@dappervision.com>
2012-09-25 19:54:48 -0400

    Updated
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- hadoopy_rt/sum_job.py
+++ hadoopy_rt/sum_job.py
@@ -6,19 +6,10 @@ import hadoopy_rt
 class Mapper(object):
 
     def __init__(self):
-        self.sum = {}
-    
-    @hadoopy_rt.close_on_flush
-    def map(self, key, value):
-        try:
-            self.sum[key] += value
-        except KeyError:
-            self.sum[key] = value
+        pass
 
-    def close(self):
-        for kv in self.sum.items():
-            yield kv
-        self.sum = {}
+    def map(self, key, value):
+        print((key, value))
 
 if __name__ == '__main__':
     hadoopy.run(Mapper)
